3c7979345ea0abeff7c9d606b4540adf7bb48ced
SantiagoREvangelista <61833820+SantiagoREvangelista@users.noreply.github.com>
2020-08-02 13:10:59 +0200

    Add files via upload

--- in_dev/App.js
+++ in_dev/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as Permissions from 'expo-permissions';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
-import CameraPage from './camera.page';
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <CameraPage />
-        );
-    };
+  useEffect(() => {
+    (async () => {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} type={type}>
+      </Camera>
+    </View>
+  );
 }
